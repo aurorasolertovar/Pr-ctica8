f89db4e115adda29c4c28a007380fd692c52c238
aurorasolertovar <aurora.fi1234@gmail.com>
2026-04-14 13:42:42 -0600

    Create programa4.c

--- programa4.c
+++ programa4.c
@@ -18,24 +18,31 @@ int main ()
             printf("\tSumar\n");
             printf("Introduzca los números a sumar separados por comas\n"); scanf("%d, %d",&uno, &dos);
             printf("%d + %d = %d\n", uno, dos, (uno + dos));
-            break; case 2:
-printf("\tRestar\n");
-printf("Introduzca los números a restar separados por comas\n"); scanf("%d, %d",&uno, &dos);
-printf("%d - %d = %d\n", uno, dos, (uno - dos));
-break;
-case 3:
-printf("\tMultiplicar\n");
-printf("Introduzca los números a multiplicar separados por comas\n"); scanf("%d, %d",&uno, &dos);
-printf("%d * %d = %d\n", uno, dos, (uno * dos));
-break;
-case 4:
-printf("\tDividir\n");
-printf("Introduzca los números a dividir separados por comas\n");
-scanf("%d, %d",&uno, &dos);
-printf("%d / %d = %.2lf\n", uno, dos, ((double)uno / dos)); break;
-
-case 5: printf("\tSalir\n"); break;
-default:
-printf("\tOpción inválida.\n");
-} }
-while (op != 5); }
+            break; 
+        case 2:
+            printf("\tRestar\n");
+            printf("Introduzca los números a restar separados por comas\n"); 
+            scanf("%d, %d",&uno, &dos);
+            printf("%d - %d = %d\n", uno, dos, (uno - dos));
+            break;
+        case 3:
+            printf("\tMultiplicar\n");
+            printf("Introduzca los números a multiplicar separados por comas\n"); 
+            scanf("%d, %d",&uno, &dos);
+            printf("%d * %d = %d\n", uno, dos, (uno * dos));
+            break;
+        case 4:
+            printf("\tDividir\n");
+            printf("Introduzca los números a dividir separados por comas\n");
+            scanf("%d, %d",&uno, &dos);
+            printf("%d / %d = %.2lf\n", uno, dos, ((double)uno / dos)); 
+            break;
+        case 5: 
+            printf("\tSalir\n"); 
+            break;
+        default:
+            printf("\tOpción inválida.\n");
+      } 
+    }
+    while (op != 5); 
+}
